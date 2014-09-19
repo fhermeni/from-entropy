3d749a6083be49c5e5e28c6f78cf866845785d2a
Vincent KHERBACHE <vincent.kherbache@inria.fr>
2014-09-19 10:31:17 +0200

    updated Launcher and tests

--- src/main/java/btrplace/benchLauncher/Launcher.java
+++ src/main/java/btrplace/benchLauncher/Launcher.java
@@ -2,15 +2,24 @@ package btrplace.benchLauncher;
 
 import btrplace.json.JSONConverterException;
 import btrplace.json.model.InstanceConverter;
+import btrplace.model.Attributes;
 import btrplace.model.Instance;
+import btrplace.model.Node;
+import btrplace.model.VM;
+import btrplace.plan.ReconfigurationPlan;
+import btrplace.plan.event.Action;
+import btrplace.solver.SolverException;
 import btrplace.solver.choco.ChocoReconfigurationAlgorithm;
 import btrplace.solver.choco.DefaultChocoReconfigurationAlgorithm;
+import btrplace.solver.choco.transition.KeepRunningVM;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by vkherbac on 16/09/14.
@@ -19,12 +28,11 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        String src, dst = null;
         if (args.length < 3 || args.length > 3 || !args[args.length-2].equals("-o")) {
             usage(1);
         }
-        src = args[0];
-        dst = args[1];
+        String src = args[0];
+        String dst = args[2];
 
         // Read the input JSON instance
         JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
@@ -47,7 +55,33 @@ public class Launcher {
             e.printStackTrace();
         }
 
-        /***** Set custom attributes (static) *******
+        //Set custom attributes
+        setAttributes(i);
+
+        // Create and customize a reconfiguration algorithm
+        ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
+        //cra.setTimeLimit(300);
+        cra.doRepair(true);
+        cra.setVerbosity(1);
+
+        // Try to solve
+        ReconfigurationPlan plan = null;
+        try {
+            plan = cra.solve(i.getModel(), i.getSatConstraints());
+        } catch (SolverException e) {
+            e.printStackTrace();
+        }
+
+        // Save stats to a CSV file
+        try {
+            createCSV(dst, plan, cra);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setAttributes(Instance i) {
+
         Attributes attrs = i.getModel().getAttributes();
         for (VM vm : i.getModel().getMapping().getAllVMs()) {
             // Hypervisor
@@ -71,30 +105,48 @@ public class Launcher {
             attrs.put(n, "boot", 6);
             attrs.put(n, "shutdown", 6);
         }
-        **********************************************/
+    }
 
-        // Create and customize the reconfiguration algorithm
-        ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
-        cra.setTimeLimit(300);
-        cra.doRepair(true);
-        cra.setVerbosity(2);
+    public static void createCSV(String fileName, ReconfigurationPlan plan, ChocoReconfigurationAlgorithm cra) throws IOException {
 
-        /* Try to solve
-        ReconfigurationPlan plan = null;
-        try {
-            plan = cra.solve(i.getModel(), i.getSatConstraints());
-        } catch (SolverException e) {
-            e.printStackTrace();
+        FileWriter writer = new FileWriter(fileName);
+        writer.append("metric;value\n");
+
+        // Store plan parameters
+        if (plan != null) {
+            writer.append("planDuration;" + plan.getDuration() + '\n');
+            writer.append("planSize;" + plan.getSize() + '\n');
+            writer.append("planActionsSize;" + plan.getActions().size() + '\n');
         }
 
-        System.out.println(plan);
-        */
+        for (Action a : plan.getActions()) {
+            if (a instanceof KeepRunningVM) {
+                //writer.append("planRelocateActions;"+plan.getActions());
+            }
+        }
+        writer.append("craSolutionTime;" + cra.getStatistics().getSolutions().get(0).getTime() + '\n');
+
+        // Store reconf. algo. stats
+        writer.append("craCoreRPBuildDuration;"+String.valueOf(cra.getStatistics().getCoreRPBuildDuration())+'\n');
+        writer.append("craNbBacktracks;"+String.valueOf(cra.getStatistics().getNbBacktracks())+'\n');
+        writer.append("craNbConstraints;"+String.valueOf(cra.getStatistics().getNbConstraints())+'\n');
+        writer.append("craNbManagedVMs;"+String.valueOf(cra.getStatistics().getNbManagedVMs())+'\n');
+        writer.append("craNbNodes;"+String.valueOf(cra.getStatistics().getNbNodes())+'\n');
+        writer.append("craNbSearchNodes;"+String.valueOf(cra.getStatistics().getNbSearchNodes())+'\n');
+        writer.append("craNbVMs;"+String.valueOf(cra.getStatistics().getNbVMs())+'\n');
+        writer.append("craSolvingDuration;"+String.valueOf(cra.getStatistics().getSolvingDuration())+'\n');
+        writer.append("craNbSolutions;"+String.valueOf(cra.getStatistics().getSolutions().size())+'\n');
+        writer.append("craSpeRPDuration;"+String.valueOf(cra.getStatistics().getSpeRPDuration())+'\n');
+        writer.append("craStart;"+String.valueOf(cra.getStatistics().getStart()));
+
+        writer.flush();
+        writer.close();
     }
 
     public static void usage(int code) {
         System.out.println("Usage: converter src -o output");
         System.out.println("\tsrc: the json instance to read");
-        System.out.println("\toutput: the output JSON reconfiguration plan.");
+        System.out.println("\toutput: the output statistics file.");
         System.exit(code);
     }
 }
--- src/test/java/btrplace/benchLauncher/LauncherTest.java
+++ src/test/java/btrplace/benchLauncher/LauncherTest.java
@@ -10,32 +10,32 @@ import java.io.IOException;
 public class LauncherTest {
 
     @Test
-    public void test() throws IOException {
-
-        String[] params = new String[3];
-        params[0] = "src/test/resources/0-c33p5000-c9.json";
-        params[1] = "-o";
-        params[2] = "src/test/resources/plan_0-c33p5000-c9.json";
-        Launcher.main(params);
-    }
-
-    @Test
-    public void test2() throws IOException {
-
-        String[] params = new String[3];
-        params[0] = "src/test/resources/0.json";
-        params[1] = "-o";
-        params[2] = "src/test/resources/plan_0.json";
-        Launcher.main(params);
-    }
-
-    @Test
-    public void test3() throws IOException {
+    public void testSrc() throws IOException {
 
         String[] params = new String[3];
         params[0] = "src/test/resources/0-src.json";
         params[1] = "-o";
-        params[2] = "src/test/resources/plan_0-src.json";
-        Launcher.main(params);
+        params[2] = "src/test/resources/stats_0-src.csv";
+        //Launcher.main(params);
+    }
+
+    @Test
+    public void testSrcDst() throws IOException {
+
+        String[] params = new String[3];
+        params[0] = "src/test/resources/0.json";
+        params[1] = "-o";
+        params[2] = "src/test/resources/stats_0.csv";
+        //Launcher.main(params);
+    }
+
+    @Test
+    public void testAll() throws IOException {
+
+        String[] params = new String[3];
+        params[0] = "src/test/resources/0-c33p5000-c9.json";
+        params[1] = "-o";
+        params[2] = "src/test/resources/stats_0-c33p5000-c9.csv";
+        //Launcher.main(params);
     }
 }
